fe3499e5603fb5765f1282c08be5bd5f9fff6fce
ZeniTD12 <85677128+ZeniTD12@users.noreply.github.com>
2021-06-11 12:30:07 +0300

    Adding request for parameters

--- BuildChecker.ps1
+++ BuildChecker.ps1
@@ -1,5 +1,22 @@
-$ownerName = "" # enter name of app owner
-$appName = "" # enter name of app
-$token = "" # enter app token
+param
+(
+    # Name of app owner
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [System.String]
+    $ownerName,
+
+    # Name of app
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [System.String]
+    $appName,
+
+    # App token
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [System.String]
+    $token
+)
 
 Invoke-WebRequest -Uri "https://api.appcenter.ms/v0.1/apps/$ownerName/$appName/branches" -Method "GET" -Headers @{"Accept"="application/json"; "X-API-Token"=$token}
