d904feb18a529ea192e0b94129eba65324745905
ZeniTD12 <85677128+ZeniTD12@users.noreply.github.com>
2021-06-11 14:05:39 +0300

    Adding displaying info about builds, but duration

--- BuildChecker.ps1
+++ BuildChecker.ps1
@@ -19,4 +19,26 @@ param
     $token
 )
 
-Invoke-WebRequest -Uri "https://api.appcenter.ms/v0.1/apps/$ownerName/$appName/branches" -Method "GET" -Headers @{"Accept"="application/json"; "X-API-Token"=$token}
+# getting branches info
+$requestResult = Invoke-WebRequest -Uri "https://api.appcenter.ms/v0.1/apps/$ownerName/$appName/branches" -Method "GET" -Headers @{"Accept"="application/json"; "X-API-Token"=$token}
+$branches = $requestResult.Content | ConvertFrom-Json
+
+# echo state of branches
+$i = 0
+foreach ($branch in $branches)
+{
+    #getting data about build logs
+    $buildId = $branch.lastbuild.id
+    $requestResult = Invoke-WebRequest -Uri "https://api.appcenter.ms/v0.1/apps/$ownerName/$appName/builds/$buildId/downloads/logs" -Method "GET" -Headers @{"Accept"="application/json"; "X-API-Token"=$token}
+    $buildUri = $requestResult.Content | ConvertFrom-Json
+    
+    $i++
+
+    #echo build data
+    Write-Host $i "-" $branch.branch.name "build" $branch.lastbuild.status "in # seconds. Link to build logs:" $buildUri.uri
+    
+    #info about duration
+    #$startTime = 
+    #$duration = $branch.lastbuild.finishTime - $branch.lastbuild.startTime
+    #Write-Host $duration
+}
