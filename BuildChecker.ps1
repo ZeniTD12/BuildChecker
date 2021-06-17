074ac145bcb9ffc90c6d922bc786cf6a84620605
ZeniTD12 <85677128+ZeniTD12@users.noreply.github.com>
2021-06-17 22:29:37 +0300

    Created functions for actions in script

--- BuildChecker.ps1
+++ BuildChecker.ps1
@@ -25,118 +25,124 @@ param
     $token
 )
 
+function Get-BranchesFromAppCenter {
+    param (
+        [System.String] $ownerName,
+        [System.String] $appName,
+        [System.String] $token
+    )
+
+    $requestResult = Invoke-WebRequest -Uri "https://api.appcenter.ms/v0.1/apps/$ownerName/$appName/branches" -Method "GET" -Headers @{"Accept"="application/json"; "X-API-Token"=$token}
+    if ($requestResult -ne $null)
+    {
+        $global:branches = $requestResult.Content | ConvertFrom-Json
+    } else {
+        Write-Host "Data hasn't been received from App Center"
+        break
+    }
+}
+
+function Get-BuildStatus {
+    Get-BranchesFromAppCenter -ownerName $ownerName -appName $appName -token $token
+
+    # echo state of branches
+    $i = 0
+    foreach ($branch in $branches)
+    {
+        $i++
+
+        #getting data about build logs
+        $buildId = $branch.lastbuild.id
+        $requestResult = Invoke-WebRequest -Uri "https://api.appcenter.ms/v0.1/apps/$ownerName/$appName/builds/$buildId/downloads/logs" -Method "GET" -Headers @{"Accept"="application/json"; "X-API-Token"=$token}
+        if ($requestResult -ne $null)
+        {
+            $buildUri = $requestResult.Content | ConvertFrom-Json
+        } else {
+            Write-Host "Info about logs hasn't been received from App Center for branch" $branch.branch.name
+        }
+              
+        # check build state, calculate duration and echo build data
+        switch ($branch.lastbuild.status)
+        {
+            "inProgress" {
+                $durationTimespan = (Get-Date) - (Get-Date -Date $branch.lastbuild.startTime)
+                $duration = [math]::Round($durationTimespan.TotalSeconds)
+                Write-Host "--------------------------------------------------"
+                Write-Host $i "-" $branch.branch.name "build is in status" $branch.lastbuild.status "for" $duration "seconds. Link to build logs:" $buildUri.uri
+                Write-Host "--------------------------------------------------"
+            }
+            "notStarted" {
+                Write-Host "--------------------------------------------------"
+                Write-Host $i "-" $branch.branch.name "build is queued ( status -" $branch.lastbuild.status" ) since" (Get-Date -Date $branch.lastbuild.queueTime)
+                Write-Host "--------------------------------------------------"
+            }
+            default {
+                $durationTimespan = (Get-Date -Date $branch.lastbuild.finishTime) - (Get-Date -Date $branch.lastbuild.startTime)
+                $duration = [math]::Round($durationTimespan.TotalSeconds)
+                Write-Host "--------------------------------------------------"
+                Write-Host $i "-" $branch.branch.name "build" $branch.lastbuild.status "in" $duration "seconds. Link to build logs:" $buildUri.uri
+                Write-Host "--------------------------------------------------"
+            }
+        }  
+        
+        # pause output after each 10 builds
+        if (([math]::IEEERemainder($i,10) -eq 0) -and ($i -lt $branches.Count))
+        {
+            Write-Host "Total number of branches is" $branches.Count ". Press any key to see next builds ..."
+            $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+        }
+    }
+
+}
+
+function Get-NewBuild {
+    Get-BranchesFromAppCenter -ownerName $ownerName -appName $appName -token $token
+      
+    # selecting branch for build
+    Write-Host "Select branch to start new build."
+    $i = 1
+    foreach ($branch in $branches)
+    {
+        Write-Host $i "-" $branch.branch.name
+        $i++
+    }
+    
+    $branchNum = Read-Host "Enter number of required branch"
+    $branchForBuild = $branches[$branchNum - 1].branch.name
+    $branchForBuildURL = $branchForBuild -replace "/", "%2F"
+        
+    #confirmation and starting of build
+    $confirmation = Read-Host "New build will be started in branch" $branchForBuild ". Are you sure? (y/n)"
+    Do {
+        switch ($confirmation)
+        {
+            "y" {
+                $requestResult = Invoke-WebRequest -Uri "https://api.appcenter.ms/v0.1/apps/$ownerName/$appName/branches/$branchForBuildURL/builds" -Method "POST" -Headers @{"Accept"="application/json"; "X-API-Token"=$token}
+                
+                if (($requestResult.Content | ConvertFrom-Json).id -ne $null)
+                {
+                    $queueTime = Get-Date -Date ($requestResult.Content | ConvertFrom-Json).queueTime
+                    Write-Host "New build in branch" $branchForBuild "has been started at" $queueTime
+                } else {
+                    Write-Host "Build in branch" $branchForBuild "has not been started. Please check script output to identify reason of this"
+                }
+                $finished = 1
+            }
+            "n" {
+                Write-Host "Starting of build in branch" $branchForBuild "was canceled"
+                $finished = 1
+            }
+            default {
+                $confirmation = Read-Host "Invalid answer was received.`nNew build will be started in branch" $branchForBuild ". Are you sure? (y/n)"
+            }
+        }
+    } while ($finished -ne 1)
+}
+
 switch ($reqOperation)
 {
-    "GetStatus" {
-        # getting branches info
-        $requestResult = Invoke-WebRequest -Uri "https://api.appcenter.ms/v0.1/apps/$ownerName/$appName/branches" -Method "GET" -Headers @{"Accept"="application/json"; "X-API-Token"=$token}
-        if ($requestResult -ne $null)
-        {
-            $branches = $requestResult.Content | ConvertFrom-Json
-        } else {
-            Write-Host "Data hasn't been received from App Center"
-            break
-        }
-
-        # echo state of branches
-        $i = 0
-        foreach ($branch in $branches)
-        {
-            $i++
-            
-            #getting data about build logs
-            $buildId = $branch.lastbuild.id
-            $requestResult = Invoke-WebRequest -Uri "https://api.appcenter.ms/v0.1/apps/$ownerName/$appName/builds/$buildId/downloads/logs" -Method "GET" -Headers @{"Accept"="application/json"; "X-API-Token"=$token}
-            if ($requestResult -ne $null)
-            {
-                $buildUri = $requestResult.Content | ConvertFrom-Json
-            } else {
-                Write-Host "Info about logs hasn't been received from App Center for branch" $branch.branch.name
-            }
-                  
-            # check build state, calculate duration and echo build data
-            switch ($branch.lastbuild.status)
-            {
-                "inProgress" {
-                    $durationTimespan = (Get-Date) - (Get-Date -Date $branch.lastbuild.startTime)
-                    $duration = [math]::Round($durationTimespan.TotalSeconds)
-                    Write-Host "--------------------------------------------------"
-                    Write-Host $i "-" $branch.branch.name "build is in status" $branch.lastbuild.status "for" $duration "seconds. Link to build logs:" $buildUri.uri
-                    Write-Host "--------------------------------------------------"
-                }
-                "notStarted" {
-                    Write-Host "--------------------------------------------------"
-                    Write-Host $i "-" $branch.branch.name "build is queued ( status -" $branch.lastbuild.status" ) since" (Get-Date -Date $branch.lastbuild.queueTime)
-                    Write-Host "--------------------------------------------------"
-                }
-                default {
-                    $durationTimespan = (Get-Date -Date $branch.lastbuild.finishTime) - (Get-Date -Date $branch.lastbuild.startTime)
-                    $duration = [math]::Round($durationTimespan.TotalSeconds)
-                    Write-Host "--------------------------------------------------"
-                    Write-Host $i "-" $branch.branch.name "build" $branch.lastbuild.status "in" $duration "seconds. Link to build logs:" $buildUri.uri
-                    Write-Host "--------------------------------------------------"
-                }
-            }
-            
-            # pause output after each 10 builds
-            if (([math]::IEEERemainder($i,10) -eq 0) -and ($i -lt $branches.Count))
-            {
-                Write-Host "Total number of branches is" $branches.Count ". Press any key to see next builds ..."
-                $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
-            }
-        }
-    }
-    "NewBuild" {
-        # getting branches info
-        $requestResult = Invoke-WebRequest -Uri "https://api.appcenter.ms/v0.1/apps/$ownerName/$appName/branches" -Method "GET" -Headers @{"Accept"="application/json"; "X-API-Token"=$token}
-        if ($requestResult -ne $null)
-        {
-            $branches = $requestResult.Content | ConvertFrom-Json
-        } else {
-            Write-Host "Data hasn't been received from App Center"
-        }
-
-        # selecting branch for build
-        Write-Host "Select branch to start new build."
-
-        $i = 1
-        foreach ($branch in $branches)
-        {
-            Write-Host $i "-" $branch.branch.name
-            $i++
-        }
-
-        $branchNum = Read-Host "Enter number of required branch"
-        $branchForBuild = $branches[$branchNum - 1].branch.name
-        $branchForBuildURL = $branchForBuild -replace "/", "%2F"
-        
-        #confirmation and starting of build
-        $confirmation = Read-Host "New build will be started in branch" $branchForBuild ". Are you sure? (y/n)"
-        Do {
-            switch ($confirmation)
-            {
-                "y" {
-                    $requestResult = Invoke-WebRequest -Uri "https://api.appcenter.ms/v0.1/apps/$ownerName/$appName/branches/$branchForBuildURL/builds" -Method "POST" -Headers @{"Accept"="application/json"; "X-API-Token"=$token}
-                    
-                    if (($requestResult.Content | ConvertFrom-Json).id -ne $null)
-                    {
-                        $queueTime = Get-Date -Date ($requestResult.Content | ConvertFrom-Json).queueTime
-                        Write-Host "New build in branch" $branchForBuild "has been started at" $queueTime
-                    } else {
-                        Write-Host "Build in branch" $branchForBuild "has not been started. Please check script output to identify reason of this"
-                    }
-                    $finished = 1
-                }
-                "n" {
-                    Write-Host "Starting of build in branch" $branchForBuild "was canceled"
-                    $finished = 1
-                }
-                default {
-                    $confirmation = Read-Host "Invalid answer was received.`nNew build will be started in branch" $branchForBuild ". Are you sure? (y/n)"
-                }
-            }
-        } while ($finished -ne 1)
-    }
+    "GetStatus" {Get-BuildStatus}
+    "NewBuild" {Get-NewBuild}
     default {
         Write-Host "Unexpected operation was requested.`nUse 'GetStatus' to receive status of latest builds.`nUse 'NewBuild' to start new build."
     }
